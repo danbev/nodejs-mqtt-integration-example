078002033ce58d9c83f9a4b00cb9f6b700cebcbf
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-06-29 16:40:59 +0200

    Add updateResponse handling and refactoring

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-import processor from './lib/processor.js';
-import server from './lib/server.js';
+import mqtt from './lib/mqtt-server.js';
+import httpserver from './lib/http-server.js';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-const dir = path.join(dirname(fileURLToPath(import.meta.url)), 'static');
-const s = server.init(8080, '0.0.0.0', dir);
-processor.start(s);
+const httpDir = path.join(dirname(fileURLToPath(import.meta.url)), 'static');
+
+class Sse {
+
+  constructor(command) {
+    this.lastResponse = command;
+    this.httpServer = null;
+  }
+
+  sendMessageEvent(event) {
+    this.httpServer.sendMessageEvent(event);
+  }
+
+  sendResponseEvent(event) {
+    this.httpServer.sendResponseEvent(event);
+  }
+
+  sendCommandEvent(event) {
+    this.httpServer.sendCommandEvent(event);
+  }
+
+  updateResponse(data) {
+    this.lastResponse = data;
+    this.sendResponseEvent(this.lastResponse);
+  }
+
+}
+
+const sse = new Sse('pong');
+httpserver.init(8080, '0.0.0.0', httpDir, sse);
+mqtt.init(sse);
